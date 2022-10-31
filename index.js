6d03457b86e752e03596d31b5d1a5c13dafbd43c
Saqib <iamsaqib24@gmail.com>
2022-10-31 19:21:37 +0530

    wiring connected

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("hello");
+
+translateButton = document.querySelector('#translate-btn');
+
+function clickHandler() {
+    console.log(translateButton);
+}
+
+translateButton.addEventListener('click', clickHandler);
